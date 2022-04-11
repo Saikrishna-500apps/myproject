2d559fc521c62031a8bb8103307ba3d9aa21e2da
teja-500apps <saikrishna.kovuru@mantragroup.com>
2022-04-11 19:14:12 +0530

    commit

---
+++ src/components/tej.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <h1>{{hi}}</h1>
+        <button @click="f1()">
+
+click
+
+</button>
+
+</div>
+
+</template>
+<script>
+
+export default {
+
+name:"NamE",
+
+data() {
+
+return {
+
+message: 'Hello World!',
+
+a:[1,2,3]
+
+}
+
+},
+
+mounted(){console.log("hi")},
+
+methods:{
+
+f1(){
+
+//document.write(this.a[0]),
+
+alert("hi")
+
+console.log(this.a[1])
+
+}
+
+}
+
+}
+
+</script
