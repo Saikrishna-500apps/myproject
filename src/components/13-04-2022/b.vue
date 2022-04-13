b52633c2d8af49f03cd2bcd6270eaff3a979a4f2
teja-500apps <saikrishna.kovuru@mantragroup.com>
2022-04-13 18:57:24 +0530

    task(13-04-2022)

---
+++ src/components/13-04-2022/b.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>{{message}}
+            <button @click="f1()">
+                click
+            </button>
+            </div>
+</template>
+<script>
+    export default{
+        name:"NamE",
+        data(){
+            return{
+                message:'krishna',
+                a:[5,6,4,8]
+            
+            }
+        },
+        mounted()
+    }
+</script>
