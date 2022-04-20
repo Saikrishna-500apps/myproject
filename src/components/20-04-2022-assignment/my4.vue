02911b5dec312c656ad362391c388b0d5b3c7129
teja-500apps <saikrishna.kovuru@mantragroup.com>
2022-04-20 18:54:46 +0530

    task(20-04-2022)

---
+++ src/components/20-04-2022-assignment/my4.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <button @click="handle()">click</button>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "SeP",
+
+  methods: {
+    handle() {
+      try {
+        this.out = this.count * 5;
+
+        document.write("printing count ", this.count);
+      } catch {
+        document.write("count ");
+      }
+    },
+  },
+};
+</script>
