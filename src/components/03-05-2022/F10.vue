5e3f17328ed4f428349cc8a5913cd51c689dcae3
teja-500apps <saikrishna.kovuru@mantragroup.com>
2022-05-04 18:55:07 +0530

    task(04-05-2022)

---
+++ src/components/03-05-2022/F10.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h4>Removing undefined and values from an array</h4>
+    <button @click="fun()">Click</button><br /><br />
+    Output:{{ res }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SaI",
+
+  data() {
+    return {
+      res: " ",
+    };
+  },
+
+  methods: {
+    fun() {
+      function filterarray(test_array) {
+        let index = -1;
+        const arrlength = test_array ? test_array.length : 0;
+        let Index1 = -1;
+        const result = [];
+        while (++index < arrlength) {
+          const value = test_array[index];
+          if (value) {
+            result[++Index1] = value;
+          }
+        }
+        return result;
+      }
+      return (this.res = filterarray([NaN,0,15,false,-22,"",undefined,47,null]));
+    },
+  },
+};
+</script>
