6a3b4ff16313a31fb62238c66d9dc2cd9e19a18a
teja-500apps <saikrishna.kovuru@mantragroup.com>
2022-05-10 19:28:14 +0530

    10-05-2022task

---
+++ src/components/10-05-2022/p.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <b-button size="l" variant="info" class="float-center" @click="Add">Add</b-button>
+       <b-table striped hover :items="items" :fields="fields"></b-table>
+         <b-form id="form">
+                 product_name:<b-form-input id="name"  class="name" placeholder="Enter product name" required></b-form-input>
+                 price:<b-form-input id="price" class="prices"  placeholder="Enter product price:" required ></b-form-input>
+                 Product_category:<b-form-input id="category" class="categories" placeholder="Enter product category" required ></b-form-input>
+    </b-form>
+  </div>
+</template>
+<script>
+export default {
+  name: "SaI",
+  data() {
+    return {
+      items: [
+        {
+          product_name: "iphone",
+          price: "100000",
+          product_category: "mobile",
+        },
+        {
+          product_name: "samsung",
+          price: "55000",
+          product_category: "television",
+        },
+      ],
+      fields: [],
+    };
+  },
+  methods: {
+    Add() {
+      this.items.push({
+        product_name: document.getElementById("name").value,
+        price: document.getElementById("price").value,
+        product_category: document.getElementById("category").value,
+      });
+    },
+  },
+};
+</script>
