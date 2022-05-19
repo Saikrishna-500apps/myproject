01f0e7a23aa3b28d964b296979885b6a78c5b34b
teja-500apps <saikrishna.kovuru@mantragroup.com>
2022-05-19 20:46:50 +0530

    task(19-05-2022)

---
+++ src/components/18-05-2022/DisplaY.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <h2>hello</h2>
+      <button @click="fun()">click</button>
+      {{res}}
+      
+  </div>
+</template>
+
+<script>
+export default {
+    name:
+
+}
+</script>
+
+<style>
+
+</style>
