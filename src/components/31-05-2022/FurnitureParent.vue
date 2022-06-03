a1e32bcb8bfd9e66eebc501a51651c16bdebe2b7
teja-500apps <saikrishna.kovuru@mantragroup.com>
2022-06-03 16:23:55 +0530

    TaskS(03-06-2022)

---
+++ src/components/31-05-2022/FurnitureParent.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+      <FurnitureC
+        :columns="['⬜','Id', 'Product', 'Sku', 'Type', 'Quality', 'Operation']"
+        :form-fields="{}" >
+        <template v-slot:input-fields="{ formdata }"><br/>
+          Id:
+          <b-form-input
+            id="input-1"
+            v-model="formdata.Id"
+            type="number"
+            required
+            placeholder="Enter Id"
+          ></b-form-input>
+          Product:
+          <b-form-input
+            id="input-1"
+            v-model="formdata.Product"
+            required
+            placeholder="Product Name"
+          ></b-form-input>
+          Sku:
+          <b-form-input
+            v-model="formdata.Sku"
+            :options="options"
+            placeholder="Enter Sku"
+          ></b-form-input>
+          Type:
+          <b-form-input
+            id="input-1"
+            v-model="formdata.Type"
+            required
+            placeholder="Product Type"
+          ></b-form-input>
+          Quality:
+          <b-form-input
+            id="input-1"
+            v-model="formdata.Quality"
+            type="number"
+            required
+            placeholder="Enter Quality"
+          ></b-form-input>
+        </template>
+      </FurnitureC><br/><br/>
+  </div>
+</template>
+<script>
+import FurnitureC from "./FurnitureChild.vue";
+export default {
+  name: "FurnitureData",
+  components: { FurnitureC },
+  data() {
+    return {
+       
+    };
+  },
+  methods: {},
+};
+</script>
